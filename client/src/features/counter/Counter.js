35221d8e6de5936643c4da0592acacf486f917dc
jkol7 <jkolcun7@gmail.com>
2022-11-23 16:09:14 -0500

    Counter with redux

---
+++ client/src/features/counter/Counter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <sections>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+    </sections>
+  );
+};
+
+export default Counter;
